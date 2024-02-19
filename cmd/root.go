98be69f7bd7692f715456cd6dae97a7a31f8f6d3
breadrock <breadrock1@gmail.com>
2024-02-19 20:37:31 +0300

    chore(watcher): added ability to get tokens and store doc chunks

--- cmd/root.go
+++ cmd/root.go
@@ -1,35 +1,45 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
-
+	"doc-notifier/internal/pkg/server"
 	"doc-notifier/internal/pkg/watcher"
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	"strconv"
+	"strings"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "internal",
-	Short: "Launch internal service to load files from watcher directory",
+	Short: "Launch internal service to load endpoints from watcher directory",
 	Long: `
-		Launch internal service to load files from watcher directory.
+		Launch internal service to load endpoints from watcher directory.
 	`,
 
 	Run: func(cmd *cobra.Command, args []string) {
-		assistantAddr, _ := cmd.Flags().GetString("assistant-address")
-		searcherAddr, _ := cmd.Flags().GetString("searcher-address")
-		watcherPath, _ := cmd.Flags().GetStringArray("watcher-path")
-		fmt.Println(assistantAddr, searcherAddr, watcherPath)
+		serviceAddress, _ := cmd.Flags().GetString("watcherService-address")
+		llmServiceAddr, _ := cmd.Flags().GetString("llm-watcherService-address")
+		ocrServiceAddr, _ := cmd.Flags().GetString("recognition-watcherService-address")
+		docSearchServiceAddr, _ := cmd.Flags().GetString("docsearch-watcherService-address")
+		watcherPath, _ := cmd.Flags().GetStringArray("watcher-directory-path")
+		fmt.Println(ocrServiceAddr, docSearchServiceAddr, watcherPath)
 
-		service := watcher.New(&watcher.Options{
-			SearcherAddress:  searcherAddr,
-			AssistantAddress: assistantAddr,
-			WatchDirectories: watcherPath,
+		watcherService := watcher.New(&watcher.Options{
+			LlmServiceAddress: llmServiceAddr,
+			DocSearchAddress:  docSearchServiceAddr,
+			OcrServiceAddress: ocrServiceAddr,
+			WatchDirectories:  watcherPath,
 		})
 
-		service.RunWatcher()
+		tmp := strings.Split(serviceAddress, ":")
+		servicePort, _ := strconv.Atoi(tmp[1])
+		serverOptions := server.BuildOptions(tmp[0], servicePort)
+		httpServer := server.New(serverOptions, watcherService)
+		httpServer.RunServer()
+
+		watcherService.RunWatcher()
 	},
 }
 
@@ -43,7 +53,9 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().StringP("searcher-address", "s", "localhost:2892", "An docseacher address with port")
-	rootCmd.Flags().StringP("assistant-address", "a", "localhost:8000", "An assistant address with port")
-	rootCmd.Flags().StringArrayP("watcher-path", "p", []string{"/archiver"}, "A local directory path to watch fs-events")
+	rootCmd.Flags().StringP("service-address", "s", "0.0.0.0:2893", "fs-notifier service host address")
+	rootCmd.Flags().StringP("llm-service-address", "l", "localhost:8000", "An llm address with port")
+	rootCmd.Flags().StringP("recognition-service-address", "r", "localhost:8000", "An ocr address with port")
+	rootCmd.Flags().StringP("docsearch-service-address", "d", "localhost:2892", "An docseacher address with port")
+	rootCmd.Flags().StringArrayP("watcher-directory-path", "w", []string{"/archiver"}, "A local directory path to watch fs-events")
 }
