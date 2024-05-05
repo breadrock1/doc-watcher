3242d4259c816fcddfba649d1a59f0fc54319556
Bread White <breadrock1@gmail.com>
2024-05-05 19:22:04 +0300

    chore(searcher): Done refactoring for searcher pkg

--- internal/pkg/searcher/searcher.go
+++ internal/pkg/searcher/searcher.go
@@ -23,7 +23,7 @@ func New(address string, timeout time.Duration) *Service {
 	}
 }
 
-func (ss *Service) StoreDocument(document *reader.Document) error {
+func (s *Service) StoreDocument(document *reader.Document) error {
 	jsonData, err := json.Marshal(document)
 	if err != nil {
 		log.Println("Failed while marshaling doc: ", err)
@@ -31,11 +31,11 @@ func (ss *Service) StoreDocument(document *reader.Document) error {
 	}
 
 	reqBody := bytes.NewBuffer(jsonData)
-	targetURL := ss.Address + ServiceURL
+	targetURL := s.Address + ServiceURL
 	log.Printf("Storing document %s to elastic", document.FolderID)
 
 	mimeType := "application/json"
-	_, err = sender.SendRequest(reqBody, &targetURL, &mimeType, ss.timeout)
+	_, err = sender.SendRequest(reqBody, &targetURL, &mimeType, s.timeout)
 	if err != nil {
 		log.Println("Failed while sending request: ", err)
 		return err
