fe60265c885400f14e591461c31f378abbf37a46
breadrock <breadrock1@gmail.com>
2024-08-08 15:59:02 +0300

    fix(clean): Removed useless dirs and files

--- internal/tokenizer/langchain/forms.go
+++
@@ -1,8 +0,0 @@
-package langchain
-
-type GetTokensForm struct {
-	Text              string `json:"text"`
-	ChunkSize         int    `json:"chunk_size"`
-	ChunkOverlap      int    `json:"chunk_overlap"`
-	ReturnChunkedText bool   `json:"return_chunked_text"`
-}
