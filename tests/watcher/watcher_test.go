735439b33d39c68034b29ca245a3d521e66a93d7
breadrock <breadrock1@gmail.com>
2024-08-26 17:08:36 +0300

    feat(tests): Fixed tests after all changes

--- tests/watcher/watcher_test.go
+++ tests/watcher/watcher_test.go
@@ -59,33 +59,33 @@ func TestWatcherManager(t *testing.T) {
 	watch := native.New(watcherConf, ocrService, searcherService, tokenizerService, storeService)
 
 	t.Run("Append directory to watch", func(t *testing.T) {
-		err := watch.AppendDirectories([]string{TestcaseDirPath})
+		err := watch.Watcher.AppendDirectories([]string{TestcaseDirPath})
 		assert.NoError(t, err, "Failed while appending dir to watch")
 
-		dirs := watch.GetWatchedDirectories()
+		dirs := watch.Watcher.GetWatchedDirectories()
 		assert.Equal(t, len(dirs), 1, "Not equal appended dirs")
 
-		err = watch.RemoveDirectories([]string{TestcaseDirPath})
+		err = watch.Watcher.RemoveDirectories([]string{TestcaseDirPath})
 		assert.NoError(t, err, "Failed while detach dir to watch")
 	})
 
 	t.Run("Append multiple dirs to watch", func(t *testing.T) {
 		dirs := []string{TestcaseDirPath, IndexerDirPath}
-		err := watch.AppendDirectories(dirs)
+		err := watch.Watcher.AppendDirectories(dirs)
 		assert.NoError(t, err, "Failed while appending dir to watch")
 
-		attached := watch.GetWatchedDirectories()
+		attached := watch.Watcher.GetWatchedDirectories()
 		assert.Equal(t, len(dirs), len(attached), "Not equal appended dirs")
 
-		err = watch.RemoveDirectories(dirs)
+		err = watch.Watcher.RemoveDirectories(dirs)
 		assert.NoError(t, err, "Failed while detach dir to watch")
 	})
 
 	t.Run("Caught error while attach and detach non existing firs", func(t *testing.T) {
-		err := watch.AppendDirectories([]string{TestcaseDirPath + "any"})
+		err := watch.Watcher.AppendDirectories([]string{TestcaseDirPath + "any"})
 		assert.Error(t, err, "Failed while catching error to append")
 
-		err = watch.RemoveDirectories([]string{TestcaseDirPath + "any"})
+		err = watch.Watcher.RemoveDirectories([]string{TestcaseDirPath + "any"})
 		assert.Error(t, err, "Failed while catching error to detach")
 	})
 
